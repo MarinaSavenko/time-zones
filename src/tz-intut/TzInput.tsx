3fb5cb998297e85a9d7a6f06ede4fb17336f692e
Dmytro Voloshyn <dmytrovoloshyn@Mac.lan>
2025-01-25 12:01:00 -0700

    add-mui

--- src/tz-intut/TzInput.tsx
+++ src/tz-intut/TzInput.tsx
@@ -1,30 +1,51 @@
 import { useDispatch, useSelector } from "react-redux";
-import { City, setCity } from "../tz-citySlice/TzCitySlice.tsx";
+import { City } from "../tz-citySlice/TzCitySlice.tsx";
 import { RootState } from "../app/store.tsx";
+import {
+  FormControl,
+  InputLabel,
+  MenuItem,
+  Select,
+  SelectChangeEvent,
+  Stack,
+} from "@mui/material";
+import { useState } from "react";
+import styled from "styled-components";
+
+const StyledLabel = styled(InputLabel)`
+  padding-left: 10px;
+`;
 
 const TzInput = () => {
-  const selectedCity = useSelector(
-    (state: RootState) => state.tzCity.selectedCity
-  );
+  const [selectedCity, setCity] = useState<string>("");
   const cities = useSelector((state: RootState) => state.tzCity.cities);
 
-  const dispatch = useDispatch();
-
-  const handleChange = (e: React.ChangeEvent<HTMLSelectElement>) => {
-    dispatch(setCity(e.target.value));
+  const handleChange = (value: SelectChangeEvent<string>) => {
+    console.log({ value });
+    setCity(value.target.value);
   };
 
   return (
-    <>
-      <label>Select a city:</label>
-      <select name="city" value={selectedCity} onChange={handleChange}>
-        {cities.map((city: City) => (
-          <option key={city.city} value={city.city}>
-            {city.city}
-          </option>
-        ))}
-      </select>
-    </>
+    <Stack flex="1">
+      <FormControl>
+        <StyledLabel variant="standard" id="city-select">
+          City
+        </StyledLabel>
+        <Select
+          labelId="city-select"
+          id="demo-simple-select"
+          value={selectedCity}
+          label="City"
+          onChange={handleChange}
+        >
+          {cities.map((city: City) => (
+            <MenuItem value={city.city} key={`key_${city.city}`}>
+              {city.city}
+            </MenuItem>
+          ))}
+        </Select>
+      </FormControl>
+    </Stack>
   );
 };
 export default TzInput;
--- src/tz-main/TzMain.tsx
+++ src/tz-main/TzMain.tsx
@@ -1,13 +1,12 @@
-import TzInput from "../tz-intut/TzInput.tsx"
-
+import Stack from "@mui/material/Stack/Stack";
+import TzInput from "../tz-intut/TzInput.tsx";
 
 const TzMain = () => {
-    return(
-        <>
-            <TzInput />
-
-        </>
-
-    );
+  return (
+    <Stack direction="row" justifyContent="space-around" gap="20px">
+      <TzInput />
+      <TzInput />
+    </Stack>
+  );
 };
-export default TzMain
+export default TzMain;
