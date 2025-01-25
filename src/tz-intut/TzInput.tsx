40d00e62725c19dc62babfbc9abe76aa32a68710
Dmytro Voloshyn <dmytrovoloshyn@Mac.lan>
2025-01-25 11:17:53 -0700

    fix-build

--- src/tz-intut/TzInput.tsx
+++ src/tz-intut/TzInput.tsx
@@ -1,33 +1,30 @@
-import {useDispatch, useSelector} from "react-redux";
-import {City, setCity} from "../tz-citySlice/TzCitySlice.tsx";
-import {RootState} from "../app/store.tsx";
+import { useDispatch, useSelector } from "react-redux";
+import { City, setCity } from "../tz-citySlice/TzCitySlice.tsx";
+import { RootState } from "../app/store.tsx";
 
 const TzInput = () => {
-    const selectedCity = useSelector((state: RootState) => state.tzCity.selectedCity);
-    const cities = useSelector((state: RootState)=> state.tzCity.cities);
+  const selectedCity = useSelector(
+    (state: RootState) => state.tzCity.selectedCity
+  );
+  const cities = useSelector((state: RootState) => state.tzCity.cities);
 
-    const dispatch = useDispatch();
+  const dispatch = useDispatch();
 
-    const handleChange = (e: React.ChangeEvent<HTMLSelectElement>) => {
-        dispatch(setCity(e.target.value));
-    };
+  const handleChange = (e: React.ChangeEvent<HTMLSelectElement>) => {
+    dispatch(setCity(e.target.value));
+  };
 
-
-    return(
-        <>
-            <label>Select a city:</label>
-            <select name="city" value={selectedCity} onChange={handleChange}>
-                {cities.map((city: City) => (
-                    <option key={city.city} value={city.city}>
-                        {city.city}
-                    </option>
-                ))}
-            </select>
-        </>
-
-
-    );
-}
-export default TzInput
-
-
+  return (
+    <>
+      <label>Select a city:</label>
+      <select name="city" value={selectedCity} onChange={handleChange}>
+        {cities.map((city: City) => (
+          <option key={city.city} value={city.city}>
+            {city.city}
+          </option>
+        ))}
+      </select>
+    </>
+  );
+};
+export default TzInput;
