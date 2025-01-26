af58caaa57cd30e71db226e9e0fcf4f2145e5655
Dmytro Voloshyn <dmytrovoloshyn@Mac.lan>
2025-01-26 11:09:01 -0700

    fix-styles

--- src/tz-tableTime/TZTableTime.tsx
+++ src/tz-tableTime/TZTableTime.tsx
@@ -1,64 +1,58 @@
-
-import Table from '@mui/material/Table';
-import TableBody from '@mui/material/TableBody';
-import TableCell from '@mui/material/TableCell';
-import TableContainer from '@mui/material/TableContainer';
-import TableHead from '@mui/material/TableHead';
-import TableRow from '@mui/material/TableRow';
-import Paper from '@mui/material/Paper';
-import {useSelector} from "react-redux";
-import {RootState} from "../app/store.ts";
-import React from "react";
+import Table from "@mui/material/Table";
+import TableBody from "@mui/material/TableBody";
+import TableCell from "@mui/material/TableCell";
+import TableContainer from "@mui/material/TableContainer";
+import TableHead from "@mui/material/TableHead";
+import TableRow from "@mui/material/TableRow";
+import Paper from "@mui/material/Paper";
+import { useSelector } from "react-redux";
+import { RootState } from "../app/store.ts";
+import React, { useMemo } from "react";
 
 interface TZTableTimeProps {
-    selectedCity: string;
+  selectedCity: string;
 }
 
-
 const TZTableTime: React.FC<TZTableTimeProps> = ({ selectedCity }) => {
-    const cities = useSelector((state: RootState) => state.tzCity.cities);
-    const selectedCityData = cities.find(city => city.city === selectedCity);
-    console.log(selectedCityData);
-    const rows = Array.from({ length: 24 }, (_, index) => ({
-        number: index + 1,
-        time: selectedCityData
-            ? new Date(new Date().setHours(index, 0, 0, 0))
-                .toLocaleString("ru-RU", { timeZone: selectedCityData.timeZone })
-            : "",
-
-    }));
-    return (
-        <TableContainer component={Paper}>
-            <Table sx={{ minWidth: 650 }} aria-label="time table">
-                <TableHead>
-                    <TableRow>
-                        <TableCell align="right">#</TableCell>
-                        <TableCell align="center">Time</TableCell>
-
-                    </TableRow>
-                </TableHead>
-                <TableBody>
-                    {rows.map((row) => (
-                        <TableRow
-                            key={row.number}
-                            sx={{ '&:last-child td, &:last-child th': { border: 0 } }}
-                        >
-                            <TableCell component="th" scope="row" align="right">
-                                {row.number}
-                            </TableCell>
-                            <TableCell align="center">{row.time}</TableCell>
-
-                        </TableRow>
-                    ))}
-                </TableBody>
-            </Table>
-        </TableContainer>
-    );
+  const cities = useSelector((state: RootState) => state.tzCity.cities);
+  const selectedCityData = useMemo(
+    () => cities.find((city) => city.city === selectedCity),
+    [cities, selectedCity]
+  );
+  console.log(selectedCityData);
+  const rows = Array.from({ length: 24 }, (_, index) => ({
+    number: index + 1,
+    time: selectedCityData
+      ? new Date(new Date().setHours(index, 0, 0, 0)).toLocaleString("ru-RU", {
+          timeZone: selectedCityData.timeZone,
+        })
+      : "",
+  }));
+  return (
+    <TableContainer component={Paper}>
+      <Table aria-label="time table">
+        <TableHead>
+          <TableRow>
+            <TableCell align="right">#</TableCell>
+            <TableCell align="center">Time</TableCell>
+          </TableRow>
+        </TableHead>
+        <TableBody>
+          {rows.map((row) => (
+            <TableRow
+              key={row.number}
+              sx={{ "&:last-child td, &:last-child th": { border: 0 } }}
+            >
+              <TableCell component="th" scope="row" align="right">
+                {row.number}
+              </TableCell>
+              <TableCell align="center">{row.time}</TableCell>
+            </TableRow>
+          ))}
+        </TableBody>
+      </Table>
+    </TableContainer>
+  );
 };
 
-
-
-
-
 export default TZTableTime;
-
